87ede2897f0142a3fcc6340dad6d562637f9c261
Oliver Mutuku <olivermulei001@gmail.com>
2022-12-11 01:51:43 -0800

    corrected all betty errors

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-  
+
 /**
  * main - printing alphabet in uppercase and lowercase
  * Return: Always 0
@@ -9,14 +9,14 @@ int main(void)
 	char lowercase;
 	char uppercase;
 
-        for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
-        {
-                putchar(lowercase);
-        }
-        for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
-        {
-                putchar(uppercase);
-        }	
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		putchar(lowercase);
+	}
+	for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+	{
+		putchar(uppercase);
+	}
 	putchar('\n');
-        return (0);
+	return (0);
 }
