14dc95f3a9f7ac62b64fa70b949c7e622c3b18cb
Oliver Mutuku <olivermulei001@gmail.com>
2022-12-12 04:39:00 -0800

    printing all possible combinations of single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entery Printing all possible combination of single digit numbers
+ *
+ * Return : Always 0
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar(number + '0');
+		if (number < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
