d70b1ecbec043613372980ce52cd145bf50f1c04
Oliver Mutuku <olivermulei001@gmail.com>
2022-12-12 06:27:00 -0800

    complying with betty documentation style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-<<<<<<< HEAD
-printf("with proper grammer, but the outcome is a piece of art,");
-return (0);
-=======
-    printf("with proper grammer, but the outcome is a piece of art, ");
+	printf("with proper grammer, but the outcome is a piece of art,");
 	return (0);
->>>>>>> 047318305f0991f84000ee1a364d128b11d1958b
 }
