c545dcda4a53efcbb48878b96f9a6b6bbeba614a
Oliver Mutuku <olivermulei001@gmail.com>
2022-12-14 12:40:16 -0500

    hot fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,12 +5,12 @@
  *
  * Return: Always 0 (Success)
  */
-void print_alphabet_x10(void)
+void print_alphabet_x10(void);
 {
 	char letter;
 	int x;
 
-	for (x = 0; x <= 9; x++)
+	for (x = 0; x < 10; x++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
