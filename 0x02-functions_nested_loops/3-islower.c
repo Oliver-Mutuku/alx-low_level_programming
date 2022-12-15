15e32e2a8e11f4fa92c1aafb8fba3fb3fd05a6bd
Oliver Mutuku <olivermulei001@gmail.com>
2022-12-15 00:05:08 -0500

    added description for parameter/member c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,11 @@
 
 /**
 * main - _islower --> checks for lowercase character
-*
+* @c: a character argument
 * Return: 1 if c is lowercasen and 0 if otherwise
 *
 */
 int _islower(int c);
 {
-
 	return (c >= 'a' && c <= 'z');
 }
