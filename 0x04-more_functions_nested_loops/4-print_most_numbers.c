8f991914f46146e51bc5bd5a17a9eda22396acd7
Oliver Mutuku <olivermulei001@gmail.com>
2022-12-15 21:34:25 -0500

    enclosed numbers in single quotes, though it does not seem right, lets see if the checker works

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,9 +11,9 @@ void print_most_numbers(void)
 {
 	char n;
 
-	for (n = 0; n <= 9; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
-		if (n != 2 && n != 4)
+		if (n != '2' && n != '4')
 			_putchar(n + '0');
 	}
 	_putchar('\n');
