c58f2c5f16ca7d37f22262bfc6a68cb498af70cc
Oliver Mutuku <olivermulei001@gmail.com>
2022-12-15 20:56:50 -0500

    fix for function description coz of betty

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
-/*
-* _isdigit - check if a character is a digit
+/**
+* _isdigit - write a function that check for a digit (0 through 9).
 * @c: the character to be checked
 *
 * Return: 1 if true, 0 if otherwise
 */
 int _isdigit(int c)
 {
-	if (c >= 1 && c <= 9)
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
